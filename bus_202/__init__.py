03928b9bff88a793fbe331ea0ea1ab0da2551737
Justin G. Davis <132368537+justin-g-davis@users.noreply.github.com>
2025-01-08 11:46:41 -0500

    Create __init__.py

---
+++ bus_202/__init__.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from pathlib import Path
+
+# Get the directory containing the data
+DATA_DIR = Path(__file__).parent / 'data'
+
+# Load the data when the package is imported
+gapfinder = pd.read_csv(DATA_DIR / 'gapfinder.csv')
