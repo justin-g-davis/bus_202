cb80354222f4d3788f87c65e94912e930087af2d
Justin G. Davis <132368537+justin-g-davis@users.noreply.github.com>
2025-02-02 17:27:38 -0500

    Update __init__.py

--- bus_202/__init__.py
+++ bus_202/__init__.py
@@ -6,4 +6,6 @@ DATA_DIR = Path(__file__).parent / 'data'
 
 # Load the data when the package is imported
 gapfinder = pd.read_excel(DATA_DIR / 'gapfinder.xlsx')
+financials = pd.read_excel(DATA_DIR / 'financials.xlsx')
+exec_pay = pd.read_excel(DATA_DIR / 'exec_pay.xlsx')
 a1_df = pd.read_excel(DATA_DIR / 'a1_df.xlsx')
