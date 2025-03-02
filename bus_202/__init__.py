ab4661f8132e5a4713c478cfe68f68c35e61e71e
Justin G. Davis <132368537+justin-g-davis@users.noreply.github.com>
2025-03-02 16:54:26 -0500

    Update __init__.py

--- bus_202/__init__.py
+++ bus_202/__init__.py
@@ -4,24 +4,3 @@ from .functions.trim import trim
 from .functions.boxplot import boxplot
 from .functions.histogram import histogram
 from .data import df
-
-# Create properties at the package level
-@property
-def gapfinder():
-    return df.gapfinder
-
-@property
-def financials():
-    return df.financials
-
-@property
-def exec_comp():
-    return df.exec_comp
-
-@property
-def a1_df():
-    return df.a1_df
-
-@property
-def midterm():
-    return df.midterm
