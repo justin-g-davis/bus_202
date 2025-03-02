eaadc661c616cb57b89cf5ffd62bca5323ca3244
Justin G. Davis <132368537+justin-g-davis@users.noreply.github.com>
2025-03-02 16:56:36 -0500

    Update __init__.py

--- bus_202/__init__.py
+++ bus_202/__init__.py
@@ -3,4 +3,4 @@ from pathlib import Path
 from .functions.trim import trim
 from .functions.boxplot import boxplot
 from .functions.histogram import histogram
-from .data import df
+from .data import midterm, financials, exec_comp, a1_df, gapfinder
