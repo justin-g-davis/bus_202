927ed757b944f0dacd019734beb4bd83160b021f
Justin G. Davis <132368537+justin-g-davis@users.noreply.github.com>
2025-03-02 16:44:02 -0500

    Update __init__.py

--- bus_202/data/__init__.py
+++ bus_202/data/__init__.py
@@ -3,17 +3,41 @@ from pathlib import Path
 
 DATA_DIR = Path(__file__).parent
 
-def gapfinder():
-    return pd.read_excel(DATA_DIR / 'gapfinder.xlsx')
+class DataFrames:
+    _midterm = None
+    _financials = None
+    _exec_comp = None
+    _a1_df = None
+    
+    @property
+    def midterm(self):
+        if self._midterm is None:
+            self._midterm = pd.read_excel(DATA_DIR / 'midterm.xlsx')
+        return self._midterm
+    
+    @property
+    def financials(self):
+        if self._financials is None:
+            self._financials = pd.read_excel(DATA_DIR / 'financials.xlsx')
+        return self._financials
+    
+    @property
+    def exec_comp(self):
+        if self._exec_comp is None:
+            self._exec_comp = pd.read_excel(DATA_DIR / 'exec_comp.xlsx')
+        return self._exec_comp
+    
+    @property
+    def a1_df(self):
+        if self._a1_df is None:
+            self._a1_df = pd.read_excel(DATA_DIR / 'a1_df.xlsx')
+        return self._a1_df
 
-def financials():
-    return pd.read_excel(DATA_DIR / 'financials.xlsx')
+# Create an instance
+_df = DataFrames()
 
-def exec_comp():
-    return pd.read_excel(DATA_DIR / 'exec_comp.xlsx')
-
-def a1_df():
-    return pd.read_excel(DATA_DIR / 'a1_df.xlsx')
-
-def midterm():
-    return pd.read_excel(DATA_DIR / 'midterm.xlsx')
+# Export the properties
+midterm = _df.midterm
+financials = _df.financials
+exec_comp = _df.exec_comp
+a1_df = _df.a1_df
