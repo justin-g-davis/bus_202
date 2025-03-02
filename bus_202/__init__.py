d3822fe37af4f8bc0deef32e8799b371de1427b7
Justin G. Davis <132368537+justin-g-davis@users.noreply.github.com>
2025-03-02 16:33:47 -0500

    Update __init__.py

--- bus_202/__init__.py
+++ bus_202/__init__.py
@@ -6,9 +6,3 @@ from .functions.histogram import histogram
 
 # Get the directory containing the data
 DATA_DIR = Path(__file__).parent / 'data'
-
-# Load the data when the package is imported
-financials = pd.read_excel(DATA_DIR / 'financials.xlsx')
-exec_comp = pd.read_excel(DATA_DIR / 'exec_comp.xlsx')
-a1_df = pd.read_excel(DATA_DIR / 'a1_df.xlsx')
-midterm = pd.read_excel(DATA_DIR / 'midterm.xlsx')
