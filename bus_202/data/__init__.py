8d7d1515fdbf3ae141d0c547501d27ef98daa264
Justin G. Davis <132368537+justin-g-davis@users.noreply.github.com>
2025-03-02 16:36:02 -0500

    Update __init__.py

--- bus_202/data/__init__.py
+++ bus_202/data/__init__.py
@@ -1,1 +1,16 @@
+import pandas as pd
+from pathlib import Path
 
+DATA_DIR = Path(__file__).parent
+
+def financials():
+    return pd.read_excel(DATA_DIR / 'financials.xlsx')
+
+def exec_comp():
+    return pd.read_excel(DATA_DIR / 'exec_comp.xlsx')
+
+def a1_df():
+    return pd.read_excel(DATA_DIR / 'a1_df.xlsx')
+
+def midterm():
+    return pd.read_excel(DATA_DIR / 'midterm.xlsx')
