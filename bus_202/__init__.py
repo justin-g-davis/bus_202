b13c61446390b57b7f87c52be8df2c9e53c0598c
Justin G. Davis <132368537+justin-g-davis@users.noreply.github.com>
2025-03-02 16:35:46 -0500

    Update __init__.py

--- bus_202/__init__.py
+++ bus_202/__init__.py
@@ -3,6 +3,3 @@ from pathlib import Path
 from .functions.trim import trim
 from .functions.boxplot import boxplot
 from .functions.histogram import histogram
-
-# Get the directory containing the data
-DATA_DIR = Path(__file__).parent / 'data'
