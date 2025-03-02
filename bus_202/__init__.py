bbc14118307e537a90201404e6d2bf132e870db4
Justin G. Davis <132368537+justin-g-davis@users.noreply.github.com>
2025-03-02 16:52:46 -0500

    Update __init__.py

--- bus_202/__init__.py
+++ bus_202/__init__.py
@@ -3,4 +3,25 @@ from pathlib import Path
 from .functions.trim import trim
 from .functions.boxplot import boxplot
 from .functions.histogram import histogram
-from .data import gapfinder, financials, exec_comp, a1_df, midterm
+from .data import df
+
+# Create properties at the package level
+@property
+def gapfinder():
+    return df.gapfinder
+
+@property
+def financials():
+    return df.financials
+
+@property
+def exec_comp():
+    return df.exec_comp
+
+@property
+def a1_df():
+    return df.a1_df
+
+@property
+def midterm():
+    return df.midterm
