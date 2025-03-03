96133f80684f0fa9b0751b423d3c33887f9cfbe9
Justin G. Davis <132368537+justin-g-davis@users.noreply.github.com>
2025-03-02 21:49:54 -0500

    Update __init__.py

--- bus_202/__init__.py
+++ bus_202/__init__.py
@@ -5,3 +5,8 @@ from .data_visualization.boxplot import boxplot
 from .data_visualization.histogram import histogram
 from .data import midterm, financials, exec_comp, a1_df, gapfinder
 from .stats.ci import ci
+
+# Add this line to expose the functions at package level
+__all__ = ['trim', 'boxplot', 'histogram', 'ci', 
+           'midterm', 'financials', 'exec_comp', 
+           'a1_df', 'gapfinder']
