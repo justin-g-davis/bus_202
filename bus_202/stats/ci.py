b4f8cdc3932a6d05a5e3646934c6004531d8d547
Justin G. Davis <132368537+justin-g-davis@users.noreply.github.com>
2025-03-02 21:53:49 -0500

    Update ci.py

--- bus_202/stats/ci.py
+++ bus_202/stats/ci.py
@@ -1,4 +1,4 @@
-def ci(sample_mean, n, confidence_level=0.95, is_proportion=False, std=None):
+def ci(stat, n, std=None , is_proportion=False, confidence_level=0.95):
     """Returns confidence interval for a mean or proportion"""
     from scipy import stats
     if not 0 < confidence_level < 1:
@@ -8,10 +8,10 @@ def ci(sample_mean, n, confidence_level=0.95, is_proportion=False, std=None):
     p = 1 - alpha/2
     
     if is_proportion:
-        if not 0 <= sample_stat <= 1:
+        if not 0 <= stat <= 1:
             raise ValueError("Proportion must be between 0 and 1")
         
-        se = math.sqrt((sample_stat * (1 - sample_stat)) / n)
+        se = math.sqrt((stat * (1 - stat)) / n)
         margin = stats.norm.ppf(p) * se
         
     else:
@@ -22,4 +22,4 @@ def ci(sample_mean, n, confidence_level=0.95, is_proportion=False, std=None):
         se = std / math.sqrt(n)
         margin = stats.t.ppf(p, df) * se
     
-    return (round(sample_stat - margin, 3), round(sample_stat + margin, 3))
+    return (round(stat - margin, 3), round(stat + margin, 3))
