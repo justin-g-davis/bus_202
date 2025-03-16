0e68ef7f1566b87ef9bedc001d56a60b2b26aea0
Justin G. Davis <132368537+justin-g-davis@users.noreply.github.com>
2025-03-16 18:28:07 -0400

    Update __init__.py

--- bus_202/__init__.py
+++ bus_202/__init__.py
@@ -4,7 +4,7 @@ from .data_visualization.trim import trim
 from .data_visualization.boxplot import boxplot
 from .data_visualization.histogram import histogram
 from .data_visualization.scatter import scatter
-from .data import midterm, financials, exec_comp, a1_df, gapfinder, sweet_things
+from .data import midterm, financials, exec_comp, a1_df, gapfinder, sweet_things, sweet_things_simple
 from .stats.ci import ci
 from .stats.reg import reg
 
