1e2ef8a1adf93d20e233e806bab329074a7ce3a9
Justin G. Davis <132368537+justin-g-davis@users.noreply.github.com>
2025-03-16 15:50:40 -0400

    Update __init__.py

--- bus_202/__init__.py
+++ bus_202/__init__.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from .data_visualization.trim import trim
 from .data_visualization.boxplot import boxplot
 from .data_visualization.histogram import histogram
+from .data_visualization.scatter import scatter
 from .data import midterm, financials, exec_comp, a1_df, gapfinder, sweet_things
 from .stats.ci import ci
 from .stats.reg import reg
@@ -11,4 +12,4 @@ from .stats.reg import reg
 __all__ = ['trim', 'boxplot', 'histogram', 'ci', 
            'midterm', 'financials', 'exec_comp', 
            'a1_df', 'gapfinder', 'sweet_things',
-           'reg']
+           'reg', 'scatter']
