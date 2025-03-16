60381afed8e21918af2a6d618e0bebc1805e396f
Justin G. Davis <132368537+justin-g-davis@users.noreply.github.com>
2025-03-16 14:50:16 -0400

    Update __init__.py

--- bus_202/__init__.py
+++ bus_202/__init__.py
@@ -3,10 +3,12 @@ from pathlib import Path
 from .data_visualization.trim import trim
 from .data_visualization.boxplot import boxplot
 from .data_visualization.histogram import histogram
-from .data import midterm, financials, exec_comp, a1_df, gapfinder
+from .data import midterm, financials, exec_comp, a1_df, gapfinder, sweet_things
 from .stats.ci import ci
+from .stats.reg import reg
 
 # Add this line to expose the functions at package level
 __all__ = ['trim', 'boxplot', 'histogram', 'ci', 
            'midterm', 'financials', 'exec_comp', 
-           'a1_df', 'gapfinder']
+           'a1_df', 'gapfinder', 'sweet_things',
+           'reg']
