1e642942a48bcf7702d3e6a92b66e3d3772e3414
Justin G. Davis <132368537+justin-g-davis@users.noreply.github.com>
2025-04-03 16:44:33 -0400

    Update __init__.py

--- bus_202/__init__.py
+++ bus_202/__init__.py
@@ -4,12 +4,21 @@ from .data_visualization.trim import trim
 from .data_visualization.boxplot import boxplot
 from .data_visualization.histogram import histogram
 from .data_visualization.scatter import scatter
-from .data import midterm, financials, exec_comp, a1_df, a3_df, gapfinder, sweet_things, sweet_things_simple, new_ceo, restate
+from .data import (
+    midterm, financials, exec_comp, a1_df, a3_df, gapfinder, 
+    sweet_things, sweet_things_simple, new_ceo, restate,
+    group_1_1, group_1_2, group_1_3, group_1_4, group_1_5, group_1_6,
+    group_2_1, group_2_2, group_2_3, group_2_4, group_2_5, group_2_6,
+    group_3_1, group_3_2, group_3_3, group_3_4, group_3_5, group_3_6)
 from .stats.ci import ci
 from .stats.reg import reg
 
 # Add this line to expose the functions at package level
-__all__ = ['trim', 'boxplot', 'histogram', 'ci', 
-           'midterm', 'financials', 'exec_comp', 
-           'a1_df', 'a3_df', 'gapfinder', 'sweet_things',
-           'sweet_things_simple', 'reg', 'scatter']
+__all__ = [
+    'trim', 'boxplot', 'histogram', 'ci', 
+    'midterm', 'financials', 'exec_comp', 
+    'a1_df', 'a3_df', 'gapfinder', 'sweet_things',
+    'sweet_things_simple', 'reg', 'scatter',
+    'group_1_1', 'group_1_2', 'group_1_3', 'group_1_4', 'group_1_5', 'group_1_6',
+    'group_2_1', 'group_2_2', 'group_2_3', 'group_2_4', 'group_2_5', 'group_2_6',
+    'group_3_1', 'group_3_2', 'group_3_3', 'group_3_4', 'group_3_5', 'group_3_6']
