65f3783ec5a473b1968669d6d268d8b411f4bda8
Justin G. Davis <132368537+justin-g-davis@users.noreply.github.com>
2025-10-14 11:25:44 -0400

    Update __init__.py

--- bus_202/__init__.py
+++ bus_202/__init__.py
@@ -6,11 +6,12 @@ from .data_visualization.histogram import histogram
 from .data_visualization.scatter import scatter
 from .data import (
     sp1500_cross_sectional, sp1500_panel, ceo_comp, 
-    a1, netflix_content)
+    a1, netflix_content, olympic_medals, world_cup_goals)
 from .stats.ci import ci
 from .stats.reg import reg
 
 __all__ = [
     'trim', 'boxplot', 'histogram', 'ci', 
     'sp1500_cross_sectional', 'sp1500_panel', 
-    'ceo_comp', 'a1', 'netflix_content']
+    'ceo_comp', 'a1', 'netflix_content', 
+    'olympic_medals', 'world_cup_goals']
