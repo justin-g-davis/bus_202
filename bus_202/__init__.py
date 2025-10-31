e72f91553e9bdbbee9f7be9e51d982a8e281db4c
Justin G. Davis <132368537+justin-g-davis@users.noreply.github.com>
2025-10-30 21:49:44 -0400

    Update __init__.py

--- bus_202/__init__.py
+++ bus_202/__init__.py
@@ -18,4 +18,5 @@ __all__ = [
     'sp1500_cross_sectional', 'sp1500_panel', 
     'ceo_comp', 'a1', 'netflix_content', 
     'olympic_medals', 'world_cup_goals', 
-    'midterm']
+    'midterm', 'sweet_things_simple', 
+    'sweet_things']
